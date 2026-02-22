775d366063544172a530b340490c101e17c4184d
danny333star <mccdaniel868@gmail.com>
2026-02-22 17:22:50 +0000

    new buildings and terrain etc

---
+++ gitpull/Assets/Scripts/SampleScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+
+    public int testInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello "+ testInput);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
